6740a021ee445bb4f15c1ac113ab54017295766d
Deep Darji <darjideep74@gmail.com>
2024-07-10 10:22:59 +0530

    Final

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -8,23 +8,17 @@ public class UIManager : MonoBehaviour
 {
     public GameObject[] plantPrefabs; // Array to hold different plant prefabs
     public GameObject wateringCan;
-    public Button[] plantButtons; // Array to hold different plant buttons
     public Button waterButton;
     public Button timeLapseButton;
     public TimeLapse timeLapse;
 
-    private GameObject selectedPlantPrefab;
     private Vector2[] plantPositions = new Vector2[20]; // Array to store potential positions for plants
     private int currentPlantIndex = 0; // Track current index for plant positions
 
+    private bool canPlant = true; // Flag to control planting availability
+
     void Start()
     {
-        for (int i = 0; i < plantButtons.Length; i++)
-        {
-            int index = i; // Local copy of i for the lambda expression
-            plantButtons[i].onClick.AddListener(() => SelectPlant(index));
-        }
-
         waterButton.onClick.AddListener(EnableWatering);
         timeLapseButton.onClick.AddListener(ToggleTimeLapse);
 
@@ -34,26 +28,10 @@ public class UIManager : MonoBehaviour
 
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButtonDown(0) && canPlant) // Check if planting is allowed
         {
             Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            Collider2D hitCollider = Physics2D.OverlapPoint(mousePosition);
-
-            if (hitCollider != null)
-            {
-                if (hitCollider.gameObject == waterButton.gameObject)
-                {
-                    EnableWatering();
-                }
-                else if (hitCollider.gameObject == timeLapseButton.gameObject)
-                {
-                    ToggleTimeLapse();
-                }
-                else if (selectedPlantPrefab != null && currentPlantIndex < plantPositions.Length)
-                {
-                    PlantSeed();
-                }
-            }
+            PlantSeed(mousePosition);
         }
     }
 
@@ -72,29 +50,56 @@ public class UIManager : MonoBehaviour
         }
     }
 
-    void SelectPlant(int index)
+    void PlantSeed(Vector2 position)
     {
-        selectedPlantPrefab = plantPrefabs[index];
-        Debug.Log("Selected plant: " + selectedPlantPrefab.name);
-    }
+        int randomPlantIndex = UnityEngine.Random.Range(0, plantPrefabs.Length); // Use UnityEngine.Random
+        GameObject selectedPlantPrefab = plantPrefabs[randomPlantIndex];
 
-    void PlantSeed()
-    {
-        if (selectedPlantPrefab != null && currentPlantIndex < plantPositions.Length)
+        // Find closest available position
+        Vector2 spawnPosition = GetClosestAvailablePlantPosition(position);
+
+        if (spawnPosition != Vector2.zero)
         {
-            Vector2 spawnPosition = plantPositions[currentPlantIndex];
             Instantiate(selectedPlantPrefab, new Vector3(spawnPosition.x, spawnPosition.y, 0), Quaternion.identity);
             currentPlantIndex++;
         }
         else
         {
-            Debug.Log("No more positions available for planting.");
+            Debug.Log("No available position found for planting.");
         }
+    }
+
+    Vector2 GetClosestAvailablePlantPosition(Vector2 position)
+    {
+        // Find the closest available plant position to the clicked position
+        float minDistance = float.MaxValue;
+        Vector2 closestPosition = Vector2.zero;
+
+        foreach (Vector2 plantPosition in plantPositions)
+        {
+            bool positionOccupied = Physics2D.OverlapCircle(plantPosition, 0.5f); // Check if position is occupied by a collider
+
+            if (!positionOccupied)
+            {
+                float distance = Vector2.Distance(position, plantPosition);
+                if (distance < minDistance)
+                {
+                    minDistance = distance;
+                    closestPosition = plantPosition;
+                }
+            }
+        }
+
+        return closestPosition;
     }
 
     void EnableWatering()
     {
-        wateringCan.SetActive(!wateringCan.activeSelf);
+        bool isWateringActive = !wateringCan.activeSelf;
+        wateringCan.SetActive(isWateringActive);
+
+        // Update planting availability based on watering can state
+        canPlant = !isWateringActive;
     }
 
     void ToggleTimeLapse()
